b62c640b60559b9f971305a65a136a5160da45a8
Danya <fikus.danya@mail.ru>
2026-03-26 20:20:15 +0300

    fix: add token

--- auth-service/app/core/config.py
+++ auth-service/app/core/config.py
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
 
     DATABASE_URL: str = "postgresql+asyncpg://user:password@postgres:5432/auth_db"
 
-    JWT_SECRET: str
+    JWT_SECRET: str "change-me-in-production"
     JWT_ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
