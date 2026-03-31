0082bed0da407101d02d31f62dd1492fbe2a41dd
Danya <fikus.danya@mail.ru>
2026-04-01 01:55:12 +0300

    fix: add secrets for k8s

--- core-service/app/main.py
+++ core-service/app/main.py
@@ -3,7 +3,7 @@ from prometheus_fastapi_instrumentator import Instrumentator
 
 from app.items.router import router as items_router
 
-app = FastAPI(title="Core Service", version="1.0.0")
+app = FastAPI(title="Core Service!", version="1.0.0")
 
 Instrumentator().instrument(app).expose(app)
 
